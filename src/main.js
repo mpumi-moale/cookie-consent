ee936508541180fc5af5d09b3ac72c6e39f6e9a7
Mpumelelo Moale <moalempumi5@gmail.com>
2026-05-27 15:25:20 +0200

    changed text when form is submitted

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtnEl = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modelText = document.getElementById('modal-text')
 
 
 // modal pop up
@@ -17,5 +18,25 @@ modalCloseBtnEl.addEventListener('click', function() {
 // submit form
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log('form submitted')
+    modelText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="./src/assets/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+/*
+Challenge:
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+
+<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+*/ 
 })
