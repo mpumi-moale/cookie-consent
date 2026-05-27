42f8aa9f6f7846aa3f16363ddeea562d10e23fcb
Mpumelelo Moale <moalempumi5@gmail.com>
2026-05-27 15:35:12 +0200

    set time out to change text on the model

--- src/main.js
+++ src/main.js
@@ -25,18 +25,15 @@ consentForm.addEventListener('submit', function(e){
             Uploading your data to the dark web...
         </p>
     </div>`
-/*
-Challenge:
-1. Take control of the "modal-text" element. 
-2. Make it so that when a user clicks on 
-   the accept button, the HTML string below
-   is inserted into the modal-text div.
 
-<div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
-        Uploading your data to the dark web...
-    </p>
-</div>
-*/ 
+    setTimeout(function() {
+      modelText.innerHTML = `
+      <div class="modal-inner-loading">
+        <img src="./src/assets/loading.svg" class="loading">
+        <p id="uploadText">
+            Making the sale...
+        </p>
+    </div>
+      `
+    }, 1500)
 })
