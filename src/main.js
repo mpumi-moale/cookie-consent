3d4600695a00fe99e22563a1d2ba5ff80884d593
Mpumelelo Moale <moalempumi5@gmail.com>
2026-05-27 13:05:54 +0200

    made the modal pop up disappear

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 const modal = document.getElementById('modal')
+const modalCloseBtnEl = document.getElementById('modal-close-btn')
 
+
+// modal pop up
 setTimeout(function() {
   modal.style.display = 'inline'
 }, 1500)
+
+
+// close the modal pop up
+modalCloseBtnEl.addEventListener('click', function() {
+  modal.style.display = 'none'
+})
