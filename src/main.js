35bbff494c170ab1e9504d43c8cf5a68afb361fd
Mpumelelo Moale <moalempumi5@gmail.com>
2026-05-28 12:06:55 +0200

    added second

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const modal = document.getElementById('modal')
 const modalCloseBtnEl = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const modalInner = document.getElementById('modal-inner')
 
 
 // modal pop up
@@ -18,21 +19,29 @@ modalCloseBtnEl.addEventListener('click', function() {
 // submit form
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+
+    // show upload image
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="./src/assets/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
     </div>`
+
+    // changed text upload
     setTimeout(function(){
-        document.getElementById('upload-text').innerHTML = `Making the sale...`
-    }, 1500) 
-  
-/*   
-Challenge: 
-1. Use a setTimeout to make the phrase "Uploading
-   your data to the dark web" change to "Making the 
-   sale..." after 1.5 seconds.
-⚠️ Do not change the loading svg!
-*/   
+        document.getElementById('upload-text').innerText = `Making the sale...`
+    }, 1500)   
+
+    setTimeout(function(){
+        modalInner.innerHTML = `
+        <h2>Thanks you sucker! </h2>
+          <p>We just sold the rights to your eternal soul.</p>
+          <div class="idiot-gif">
+              <img src="images/pirate.gif">
+        </div>
+        
+        `
+    }, 3000)
+
 
 })
